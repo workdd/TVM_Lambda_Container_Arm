7db39c78675c02a2f32078cc526b8b69171e9cd4
최재강 <chl8273@naver.com>
2021-12-31 13:25:02 +0900

    change name

--- lambda_bulid.sh
+++ lambda_bulid.sh
@@ -1,9 +1,9 @@
-docker build -t tvm_lambda_container . --no-cache
+docker build -t tvm_lambda_container_arm . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-docker tag tvm_lambda_container $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/tvm_lambda_container
+docker tag tvm_lambda_container_arm $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/tvm_lambda_container_arm
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
-docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/tvm_lambda_container
+docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/tvm_lambda_container_arm
