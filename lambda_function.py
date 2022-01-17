4bd54dae56dbb80ab6a41220b9d2e9514dc61603
최재강 <chl8273@naver.com>
2022-01-17 21:51:54 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -12,6 +12,7 @@ print('import time: ', time.time() - import_start_time)
 def get_model(model_name, bucket_name, get_path):
     s3_client = boto3.client('s3')
     if get_path == 'tvm/':
+        print(get_path + model_name +'/model.tar')
         s3_client.download_file(bucket_name, get_path + model_name +'/model.tar', '/tmp/' + model_name + '/model.json')
         s3_client.download_file(bucket_name, get_path + model_name +'/model.params', '/tmp/' + model_name + '/model.tar')
         s3_client.download_file(bucket_name, get_path + model_name +'/model.json', '/tmp/' + model_name + '/model.params')
