fc8e90ce191457de7ae528818a99820f77e0011a
최재강 <chl8273@naver.com>
2022-01-18 12:36:40 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -13,10 +13,10 @@ def get_model(model_name, bucket_name, get_path):
     s3_client = boto3.client('s3')
     if get_path == 'tvm/':
         print(get_path + model_name +'/model.tar')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.tar', '/tmp/' + model_name + '/model.tar')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.params', '/tmp/' + model_name + '/model.params')
-        s3_client.download_file(bucket_name, get_path + model_name +'/model.json', '/tmp/' + model_name + '/model.json')
-        return '/tmp/' + model_name + '/model.json', '/tmp/' + model_name + '/model.tar', '/tmp/' + model_name + '/model.params'
+        s3_client.download_file(bucket_name, get_path + model_name +'/model.tar', '/tmp/model.tar')
+        s3_client.download_file(bucket_name, get_path + model_name +'/model.params', '/tmp/model.params')
+        s3_client.download_file(bucket_name, get_path + model_name +'/model.json', '/tmp/model.json')
+        return '/tmp/model.json', '/tmp/model.tar', '/tmp/model.params'
     else:
         s3_client.download_file(bucket_name, get_path + model_name, '/tmp/'+ model_name)
         return '/tmp/' + model_name
